6b495c75c9c1cfcc99c11692e0f11a8ce5375fcd
Nataniel Pedersen <natanielmp@gmail.com>
2014-09-19 22:13:50 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+	<meta charset="UTF-8">
+	<title>ARV - pensumbøker</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<div class="head-wrap">
+		<div class="head-elements">
+			<h1>ARV</h1>
+			<button href="#">selg bok</button>
+		</div>
+	</div>
+
+	<div class="main-wrap">
+		
+		<section id="book-row">
+			<div class="book-thumb">
+				<img src="/img/dmc.jpg" alt="">
+				<a href="#">Descrete mathematics for programming</a>
+				<p>Lorem ipsum dolor</p>
+			</div>
+			<div class="book-thumb">
+				<img src="/img/dmc.jpg" alt="">
+				<a href="#">Descrete mathematics for programming</a>
+				<p>Lorem ipsum dolor</p>
+			</div>
+			<div class="book-thumb">
+				<img src="/img/dmc.jpg" alt="">
+				<a href="#">Descrete mathematics for programming</a>
+				<p>Lorem ipsum dolor</p>
+			</div>
+			<div class="book-thumb">
+				<img src="/img/dmc.jpg" alt="">
+				<a href="#">Descrete mathematics for programming</a>
+				<p>Lorem ipsum dolor</p>
+			</div>
+		</section>
+
+	</div>
+
+</body>
+</html>
