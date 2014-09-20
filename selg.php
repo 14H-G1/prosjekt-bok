85bcf856bdfae924efc74340b88ea9139fb6a4c3
Nataniel Pedersen <natanielmp@gmail.com>
2014-09-21 00:08:41 +0200

    Sass more modular + 'sok' page

--- selg.php
+++ selg.php
@@ -10,16 +10,13 @@
 
 	<div class="head-wrap">
 		<div class="head-elements">
-			<h1>ARV</h1>
+			<h1>PEBB</h1>
 			<a class="button" href="/index.php">tilbake</a>
 			<a class="button" href="/sok.php">søk</a>
 		</div>
 	</div>
 
-	<div id="sale-wrap">
-		
-		<div class="container">
-
+	<section id="sell-book">
 			<div id="book-img">
 				<img src="http://dummyimage.com/160x215/f0f0f0/000000&text=bok" alt="">
 			</div>
@@ -32,17 +29,18 @@
 			</div>
 			
 			<a class="button" href="#">legg ut for salg</a>
+	</section>
 
-		</div>
-
-		<div class="container">
-			<h2>Hvordan fungerer dette?</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, error, in! Magnam a, officia neque iste voluptas laudantium ex, quibusdam facilis, placeat fugiat accusantium totam quo, similique minus aperiam blanditiis!</p>
-			<h2>Hvordan fjerner jeg en solgt bok?</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, ducimus. Esse officia eos earum atque, fuga facilis animi tempora autem beatae impedit ipsam debitis nam ad, distinctio laborum nulla voluptas.</p>
-			<h2>Hvordan kan dette være gratis?</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa perspiciatis illo voluptatibus culpa, eos nemo beatae ducimus, consequuntur illum obcaecati maxime, possimus cumque alias pariatur vero aperiam. Quo, rem, debitis.</p>
-		</div>
+	<section id="faq">
+		<h2>Hvordan fungerer dette?</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, error, in! Magnam a, officia neque iste voluptas laudantium ex, quibusdam facilis, placeat fugiat accusantium totam quo, similique minus aperiam blanditiis!</p>
+		
+		<h2>Hvordan fjerner jeg en solgt bok?</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, ducimus. Esse officia eos earum atque, fuga facilis animi tempora autem beatae impedit ipsam debitis nam ad, distinctio laborum nulla voluptas.</p>
+		
+		<h2>Hvordan kan dette være gratis?</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa perspiciatis illo voluptatibus culpa, eos nemo beatae ducimus, consequuntur illum obcaecati maxime, possimus cumque alias pariatur vero aperiam. Quo, rem, debitis.</p>
+	</section>
 
 			<!--
 			<div id="results">Your captured image will appear here...</div>
@@ -51,8 +49,6 @@
 				<input type=button value="Take Large Snapshot" onClick="take_snapshot()">
 			</form>
 			-->
-
-	</div>
 
 	<footer>
 		<p>Copyright &copy; G1</p>
