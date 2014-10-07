32cc1d6ff29f791a9df6fcf7a9eca23e4dd451e2
Nataniel Pedersen <natanielmp@gmail.com>
2014-10-07 13:52:02 +0200

    Added modal to sell page

--- assets/js/modalCtrl.js
+++ assets/js/modalCtrl.js
@@ -7,3 +7,17 @@ $('#open-login').click(function(e) {
         });
     e.preventDefault();
 });
+
+$('#open-upload').click(function(e) {
+    $('#upload-modal').lightbox_me({
+        centered: true,
+        onLoad: function() {
+            $('#login-modal-form').find('input:first').focus()
+            }
+        });
+    e.preventDefault();
+});
+
+$(".fa").click(function() {
+  $(this).toggleClass("toggled");
+});
