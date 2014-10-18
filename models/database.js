85d930eb454eb099dd0abba5297546b6465edccb
Akethe <marcus.alexander.dahl@gmail.com>
2014-10-18 06:41:26 +0200

    dabase handling, schemas
    addedd own schemata folder, made database handler load this folder for
    schemas. Added some documentation

--- models/database.js
+++ models/database.js
@@ -1,9 +1,21 @@
+/*
+ *	Database handling
+ *
+ *	1) Connects to MongoDB via Mongoose given an URI (see ~/config.js)
+ *	2) Loads Schemas located in ~/models/schemas/*Schema.js
+ * */
+ 
+var glob = require('glob');
+
 var mongoose		= require('mongoose')
-  , config		= require('../config.js');
+  , config		= require('../config.js')
+  , glob		= require('glob')
+  , db 			= this;
 
-this.Schema = mongoose.Schema;
+db.Schema = mongoose.Schema;
 
-// connect to db
+/*	Connects to the MongoDB using info from ~/config.js
+ * */
 mongoose.connect(config.mongodb.uri, config.mongodb.options, function (err, res) {
   if (err) {
     console.log ('ERROR connecting to: ' + config.mongodb.uri + '. ' + err);
@@ -12,10 +24,17 @@ mongoose.connect(config.mongodb.uri, config.mongodb.options, function (err, res)
   }
 });
 
-// userSchema
-var userSchema = require('../models/userSchema.js')(config, this);
-exports.userModel = mongoose.model('User', userSchema);
-
-// bookSchema
-var bookSchema = require('../models/bookSchema.js')(config, this);
-exports.bookModel = mongoose.model('Book', bookSchema);
+/*	Loads all Schemas from folder and models them using mongoose.
+ *	Passes on config and referance to this DB handler to each Model
+ *	so mongoose Schema object can be used in each
+ * */
+glob('models/schemas/*.js', {}, function (err, files) {
+  files.forEach(function(file) {
+    var schemaName = file.substring('models/schemas/'.length, file.length-('Schema.js'.length));
+    var modelName = schemaName.charAt(0).toUpperCase()+schemaName.substring(1);
+    var schema = require('../'+file)(config, db);
+    
+    exports[schemaName+"Model"] = mongoose.model(modelName, schema);
+    console.log("Export["+schemaName+"Model]: "+model+" ~/"+file);
+  });
+});	
--- models/passport.js
+++ models/passport.js
@@ -1,8 +1,19 @@
+/*
+ *      Passport authentication
+ *
+ *      1) Load needed Strategies and the DB.
+ *      2) Return a function which configures passport
+ *         and all Strategies
+ * */
+
 var LocalStrategy       = require('passport-local').Strategy
   , FacebookStrategy	= require('passport-facebook').Strategy
   , db                  = require('../models/database.js');
 
 module.exports = function(passport) {
+    /* Serializing and Deserializing,
+     * so we can keep user data through session
+     * */
     passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
@@ -11,15 +22,26 @@ module.exports = function(passport) {
             done(err, user);
         });
     });
+    /* Set up a LocalStrategy for users not connecting
+     * through a social media login
+     * */
     passport.use('local', new LocalStrategy({
         usernameField: 'email',
         passwordField: 'password',
         passReqToCallback: true
     },
+    /* This is called when a non-social media user tries to logon
+    */
     function(req, email, password, done) {
+        /* Queue the function at next process tick,
+         * this is to make sure db.userModel.findOne()
+         * gets enough time to find a user
+         * async vs. sync
+         * */
         process.nextTick(function() {
-            db.userModel.findOne({'local.email':email}, function(err, user) {
+            db.userModel.findOne({'local.email': email}, function(err, user) {
                 if (err) return done(err);
+                /* User already exists in the Database */
                 if (user) {
                     // already in use
                 }
