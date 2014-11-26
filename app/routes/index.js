a1dde3685c3ab5762725cf234ebca6444c80d303
Akethe <marcus.alexander.dahl@gmail.com>
2014-11-26 13:44:40 +0100

    Revert "Revert "removed redirect loop on index, auth on publish""
    This reverts commit 19477dd4b953e5e1e7d54af3d872b63305769be6.

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,20 +4,16 @@ module.exports = exports = function(API) {
 
 	/* prepare index logic given request and response */
 	var index = function(req, res) {
-		/* check if request is authed */
-		API.authenticated(req, res, function() {
-			/* query API to find all books, currently not paginated */
-			API.find('all books', function(err, results) {
-				if (!err) {
-					res.render('index', {
-						title: config.app.name,
-						id: 'index',
-						books: results || []
-					});
-				}
-			});
+		/* query API to find all books, currently not paginated */
+		API.find('all books', function(err, results) {
+			if (!err) {
+				res.render('index', {
+					title: config.app.name,
+					id: 'index',
+					books: results || []
+				});
+			}
 		});
-
 	};
 	return {
 		'/': { get: index }
