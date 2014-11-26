b92eb66a41d29949c276d20d555081991c070c70
Akethe <marcus.alexander.dahl@gmail.com>
2014-11-26 01:32:44 +0100

    removed redirect loop on index, auth on publish

--- app/routes/publish.js
+++ app/routes/publish.js
@@ -1,6 +1,9 @@
 module.exports = function(API) {
 	var publish = function(req, res) {
-		res.render('publish', {id: 'publish'});
+		/* check if request is authed */
+		API.authenticated(req, res, function() {
+			res.render('publish', {id: 'publish'});
+		});
 	};
 	return {
 		'/publish': {
